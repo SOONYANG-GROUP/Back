4dfb0995c7c65bcc9259f183d8ee457f754cd14b
Chae-1 <coguddlf1000@naver.com>
2023-03-20 00:32:58 +0900

    solve cors

--- src/main/java/com/campuscrew/campuscrew/CampuscrewApplication.java
+++ src/main/java/com/campuscrew/campuscrew/CampuscrewApplication.java
@@ -17,6 +17,6 @@ public class CampuscrewApplication implements WebMvcConfigurer {
 				.allowedMethods("*")
 				.allowedOrigins("*")
 				.allowedHeaders("*")
-				.allowCredentials(true);
+				.allowCredentials(false);
 	}
 }
