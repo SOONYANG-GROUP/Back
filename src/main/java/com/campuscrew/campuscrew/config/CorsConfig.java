32d043ec150e7e4f507423b712d3a5da10946e3e
Chae-1 <coguddlf1000@naver.com>
2023-03-19 23:48:34 +0900

    solve cors

--- src/main/java/com/campuscrew/campuscrew/config/CorsConfig.java
+++ src/main/java/com/campuscrew/campuscrew/config/CorsConfig.java
@@ -20,11 +20,10 @@ public class CorsConfig {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-
         config.setAllowCredentials(true); // 내서버가 응답을 할 때 json을 자바스크립트에서 처리할 수 있게 할지를 설정
         config.addAllowedHeader("*");// 모든 헤더에 대해 허용
         config.addAllowedMethod("*");//모든 http method 에 허용
-        config.addAllowedOrigin("localhost:3000");// 모든 ip에 응답을 허용
+        config.addAllowedOrigin("http://localhost:3000");// 모든 ip에 응답을 허용
         source.registerCorsConfiguration("/api/**", config);
         return new CorsFilter(source);
     }
