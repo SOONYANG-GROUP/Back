c973628e224ec4a9d1b3ece11680cc8293385d17
Chae-1 <coguddlf1000@naver.com>
2023-03-20 01:20:16 +0900

    solve cors

--- src/main/java/com/campuscrew/campuscrew/controller/UserController.java
+++ src/main/java/com/campuscrew/campuscrew/controller/UserController.java
@@ -6,10 +6,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,7 +16,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/join")
-    public String join(@RequestBody UserJoin userJoin) throws Exception {
+    public String join(@ModelAttribute UserJoin userJoin) throws Exception {
         userService.signUp(userJoin);
         return "회원가입 성공";
     }
