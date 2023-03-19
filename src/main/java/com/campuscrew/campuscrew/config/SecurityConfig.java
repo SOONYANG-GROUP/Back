82d3ce6871621d9be764eee8a56f094207ed2d8f
Chae-1 <coguddlf1000@naver.com>
2023-03-20 01:05:31 +0900

    solve cors

--- src/main/java/com/campuscrew/campuscrew/config/SecurityConfig.java
+++ src/main/java/com/campuscrew/campuscrew/config/SecurityConfig.java
@@ -55,12 +55,10 @@ public class SecurityConfig {
                 .and()
                 // 세션 사용하지 않으므로 STATELESS로 설정
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
                 .and()
 
                 //== URL별 권한 관리 옵션 ==//
                 .authorizeHttpRequests()
-
                 // 아이콘, css, js 관련
                 // 기본 페이지, css, image, js 하위 폴더에 있는 자료들은 모두 접근 가능, h2-console에 접근 가능
                 .requestMatchers("/","/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()
--- src/main/java/com/campuscrew/campuscrew/dto/UserJoin.java
+++ src/main/java/com/campuscrew/campuscrew/dto/UserJoin.java
@@ -2,12 +2,15 @@ package com.campuscrew.campuscrew.dto;
 
 import lombok.*;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Getter
 @Builder
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserJoin {
+public class UserJoin implements Serializable {
     private String username;
     private String nickname;
     private String name;
