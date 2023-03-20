c002e5e2aea625a95e1edd58e7b5b152a37a5b45
Chae-1 <coguddlf1000@naver.com>
2023-03-20 22:34:15 +0900

    md

--- src/main/java/com/campuscrew/campuscrew/jwt/JwtAuthenticationProcessingFilter.java
+++ src/main/java/com/campuscrew/campuscrew/jwt/JwtAuthenticationProcessingFilter.java
@@ -31,10 +31,10 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         log.info("request.getRequestURI() = {}", request.getRequestURI());
-        if (request.getRequestURI().equals(NO_CHECK_URL)) {
-            filterChain.doFilter(request, response);
-            return;
-        }
+//        if (request.getRequestURI().equals(NO_CHECK_URL)) {
+//            filterChain.doFilter(request, response);
+//            return;
+//        }
         // 사용자 요청 헤더에서 RefreshToken 추출
         // -> RefreshToken이 없거나 유효하지 않다면(DB에 저장된 RefreshToken과 다르다면) null을 반환
         // 사용자의 요청 헤더에 RefreshToken이 있는 경우는, AccessToken이 만료되어 요청한 경우밖에 없다.
