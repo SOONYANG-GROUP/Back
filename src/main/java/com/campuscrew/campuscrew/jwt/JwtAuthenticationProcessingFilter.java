3cf6e6f003f73e2cf7a475aeb274f386e73dd477
Chae-1 <coguddlf1000@naver.com>
2023-03-21 00:37:51 +0900

    d

--- src/main/java/com/campuscrew/campuscrew/jwt/JwtAuthenticationProcessingFilter.java
+++ src/main/java/com/campuscrew/campuscrew/jwt/JwtAuthenticationProcessingFilter.java
@@ -39,9 +39,6 @@ public class JwtAuthenticationProcessingFilter extends OncePerRequestFilter {
         // -> RefreshToken이 없거나 유효하지 않다면(DB에 저장된 RefreshToken과 다르다면) null을 반환
         // 사용자의 요청 헤더에 RefreshToken이 있는 경우는, AccessToken이 만료되어 요청한 경우밖에 없다.
         // 따라서, 위의 경우를 제외하면 추출한 refreshToken은 모두 null
-        String refreshToken1 = jwtService.extractRefreshToken(request).get();
-
-        log.info("{}", refreshToken1);
         String refreshToken = jwtService.extractRefreshToken(request)
                 .filter(jwtService::isTokenValid)
                 .orElse(null);
