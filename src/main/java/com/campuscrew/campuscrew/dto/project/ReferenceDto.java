c4d367778c04c5f37e949ab18206c554a2ea1d7e
Chae-1 <coguddlf1000@naver.com>
2023-03-24 21:59:32 +0900

    d

--- src/main/java/com/campuscrew/campuscrew/dto/project/ReferenceDto.java
+++ src/main/java/com/campuscrew/campuscrew/dto/project/ReferenceDto.java
@@ -1,4 +1,14 @@
 package com.campuscrew.campuscrew.dto.project;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReferenceDto {
+    private String url;
 }
