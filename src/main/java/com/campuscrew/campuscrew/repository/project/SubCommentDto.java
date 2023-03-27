78aef599192b46e6976ac97a265afe3e47f5782a
hyeongil <coguddlf1000@naver.com>
2023-03-27 11:24:12 +0900

    d

--- src/main/java/com/campuscrew/campuscrew/repository/project/SubCommentDto.java
+++ src/main/java/com/campuscrew/campuscrew/repository/project/SubCommentDto.java
@@ -3,24 +3,27 @@ package com.campuscrew.campuscrew.repository.project;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 public class SubCommentDto {
     private Long subCommentId;
     private String name;
-    @JsonSerialize(using = LocalDateSerializer.class)
-    @JsonDeserialize(using = LocalDateDeserializer.class)
-    private LocalDate createDate;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime createDate;
     private String subComment;
 
-    public SubCommentDto(Long subCommentId, String name, LocalDate createDate, String subComment) {
+    public SubCommentDto(Long subCommentId, String name, LocalDateTime createDate, String subComment) {
         this.subCommentId = subCommentId;
         this.name = name;
         this.createDate = createDate;
