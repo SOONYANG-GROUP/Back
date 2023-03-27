da1948a37bb6d65d3de0a8e945b54a8d0e49cdca
hyeongil <coguddlf1000@naver.com>
2023-03-27 11:11:07 +0900

    d

--- src/main/java/com/campuscrew/campuscrew/repository/project/SubCommentDto.java
+++ src/main/java/com/campuscrew/campuscrew/repository/project/SubCommentDto.java
@@ -6,10 +6,12 @@ import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class SubCommentDto {
 
