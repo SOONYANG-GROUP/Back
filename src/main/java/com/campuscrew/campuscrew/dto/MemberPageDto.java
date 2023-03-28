6a3dcec612ed9a8e8b1e77ea6afa86fc3e2255a7
hyeongil <coguddlf1000@naver.com>
2023-03-28 15:08:03 +0900

    d

--- src/main/java/com/campuscrew/campuscrew/dto/MemberPageDto.java
+++ src/main/java/com/campuscrew/campuscrew/dto/MemberPageDto.java
@@ -1,5 +1,6 @@
 package com.campuscrew.campuscrew.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,8 @@ import java.util.List;
 
 @Setter
 @Getter
+@AllArgsConstructor
+
 public class MemberPageDto {
     private String openChatUrl;
     private String voiceChatUrl;
--- src/main/java/com/campuscrew/campuscrew/dto/ParticipatedUserDto.java
+++ src/main/java/com/campuscrew/campuscrew/dto/ParticipatedUserDto.java
@@ -1,10 +1,12 @@
 package com.campuscrew.campuscrew.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class ParticipatedUserDto {
     private String detailField;
     private String name;
