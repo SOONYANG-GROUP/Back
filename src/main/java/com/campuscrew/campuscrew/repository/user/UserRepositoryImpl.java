8bc2e87f930f8d9d4da5ec60f07edfa85e68d7cf
Chae-1 <coguddlf1000@naver.com>
2023-03-29 20:04:10 +0900

    d

--- src/main/java/com/campuscrew/campuscrew/repository/user/UserRepositoryImpl.java
+++ src/main/java/com/campuscrew/campuscrew/repository/user/UserRepositoryImpl.java
@@ -30,8 +30,8 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
     public ProfileDto fetchProfile(Long id) {
 
         List<ProfileDto> profileDtos = queryFactory.selectFrom(participatedUsers)
-                .join(participatedUsers.project, project)
-                .join(participatedUsers.user, user)
+                .join(participatedUsers.project, project).on(project.user.id.eq(participatedUsers.user.id))
+                .join(participatedUsers.user, user).on(user.id.eq(participatedUsers.user.id))
                 .distinct()
                 .where(user.id.eq(id))
                 .transform(groupBy(project.projectStatus)
