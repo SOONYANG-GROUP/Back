4cac0f72680f373a0f63afb70c730de4857a7539
Chae-1 <coguddlf1000@naver.com>
2023-04-04 21:55:27 +0900

    d

--- src/test/java/com/campuscrew/campuscrew/CampuscrewApplicationTests.java
+++ src/test/java/com/campuscrew/campuscrew/CampuscrewApplicationTests.java
@@ -1,10 +1,13 @@
 package com.campuscrew.campuscrew;
 
+import com.campuscrew.campuscrew.domain.board.ParticipatedUsers;
 import com.campuscrew.campuscrew.domain.board.Project;
 import com.campuscrew.campuscrew.domain.board.Recruit;
 import com.campuscrew.campuscrew.dto.project.ProjectMainDto;
+import com.campuscrew.campuscrew.repository.ParticipatedUsersRepository;
 import com.campuscrew.campuscrew.repository.project.CommentPageDto;
 import com.campuscrew.campuscrew.repository.project.ProjectRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,11 +19,16 @@ import java.util.List;
 @SpringBootTest
 class CampuscrewApplicationTests {
 
-
+	@Autowired
+	ParticipatedUsersRepository repository;
 
 	@Test
 	void contextLoads() {
+		ParticipatedUsers participatedUsers = repository
+				.findByUsersIdAndProjectId(4L, 1L)
+				.orElse(null);
 
+		Assertions.assertThat(participatedUsers).isNotNull();
 	}
 
 	@Test
