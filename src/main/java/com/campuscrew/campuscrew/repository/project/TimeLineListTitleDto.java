dc08bd7f07ddee3af4a0f653b48952eb486e4ef5
Chae-1 <coguddlf1000@naver.com>
2023-04-28 21:14:10 +0900

    d

--- src/main/java/com/campuscrew/campuscrew/repository/project/TimeLineListTitleDto.java
+++ src/main/java/com/campuscrew/campuscrew/repository/project/TimeLineListTitleDto.java
@@ -1,5 +1,6 @@
 package com.campuscrew.campuscrew.repository.project;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class TimeLineListTitleDto {
     private Long timeLineId;
     private String title;
